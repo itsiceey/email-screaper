d35ea3e7bddd5236197767eeebd85e20d54a5f7a
itsiceey <145792113+itsiceey@users.noreply.github.com>
2023-09-22 23:44:47 +0700

    Add files via upload

---
+++ emailscraper.py
@@ -0,0 +1,56 @@
+from collections import deque
+import re
+
+from bs4 import BeautifulSoup
+import requests
+import urllib.parse
+
+user_url = str(input('[+] Add url: '))
+urls = deque([user_url])
+scraped_urls = set()
+emails = set()
+count = 0
+limit = int(input('[+] Add Search Limit: '))
+
+try:
+	while True:
+		count += 1
+		if count > limit:
+			break
+		
+		url = urls.popleft()
+		scraped_urls.add(url)
+		parts = urllib.parse.urlsplit(url)
+		base_url = f'{parts.scheme}://{parts.netloc}'
+		path = url[:url.rfind('/')+1] if '/' in parts.path else url
+
+		print(f'{count} Processing {url}')
+
+		try:
+			response = requests.get(url)
+		except(requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+			continue
+
+		new_emails = set(re.findall(r'[a-z0-9\.\-+_]+@\w+\.+[a-z\.]+', response.text, re.I))
+		emails.update(new_emails)
+
+		soup = BeautifulSoup(response.text, 'html.parser')
+		for anchor in soup.find_all('a'):
+			link = anchor.attrs['href'] if 'href' in anchor.attrs else ''
+			if link.startswith('/'):
+				link = base_url + link
+			elif not link.startswith('http'):
+				link = path + link
+
+			if not link in urls and not link in scraped_urls:
+				urls.append(link)
+
+except KeyboardInterrupt:
+	print('[-] Closing!')
+print('\n Process Done!!')
+print(f'\n{len(emails)} email found \n ======================================')
+
+for mail in emails:
+	print('  '+mail)
+print('\n')
+
